336ff74e727b0924140beeff0e3531d998a61131
Rogério Jorge <rogeriodejesusjorge@gmail.com>
2019-09-15 20:27:14 -0400

    Plotting coils with finite square thickness

--- senac.sh
+++ senac.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # version 0.1 - R. Jorge IREAP/UMD September 2019
-proj="LHD"; # project name for input/output files, with vmec output vmec/wout_"proj".nc
+proj="W7X"; # project name for input/output files, with vmec output vmec/wout_"proj".nc
 #================
 currentDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 surfInput=${currentDIR}"/surf_input.txt"; #input file with surface parameters
@@ -10,53 +10,56 @@ vmecOutput=${currentDIR}"/vmec/${proj}/wout_${proj}.nc"; #VMEC output file to re
 runSENAC=1;              #1-> runs SENAC mathematica
 outputToVMEC=0;          #compute Fourier Modes and output to VMEC
 plotFit=1;               #Mathematica plots fit results
-plotOriginal=1;          #Mathematica plots original surface
+plotOriginal=0;          #Mathematica plots original surface
 #======VMEC=====
 runVMECofFit=0;          #run VMEC for fit
 #======REGCOIL=====
 runREGCOILfit=0;         #run REGCOIL for fit
 plotRegcoilFit=0;        #Mathematica plots coils for fit
 runREGCOILoriginal=0;    #run REGCOIL for original/VMEC file
-plotRegcoilOriginal=0;   #Mathematica plots coils for original surface
+plotRegcoilOriginal=10   #Mathematica plots coils for original surface
 #======SENAC INPUT PARAMETERS=====
 ordern=2;                #Near-Axis Expansion Order (has to be greater than 2)
-nModes=0;                #number of fourier components in mu, delta and B0
-nsurfaces=1;             #number of surfaces to read and compare from VMEC
-nthetaM=15;              #resolution in theta for fit and Mercier's coordinates
-nphiM=20;                #resolution in phi for fit and Mercier's coordinates
+nModes=3;                #number of fourier components in mu, delta and B0
+nsurfaces=6;             #number of surfaces to read and compare from VMEC
+nthetaM=35;              #resolution in theta for fit and Mercier's coordinates
+nphiM=60;                #resolution in phi for fit and Mercier's coordinates
 maxiterations=2000;      #max number of iterations for fit
 deltac0=1.1;             #initial point for deltac0 betweeon -pi and pi
 deltal0=1.0;             #initial point for deltal
 deltalmin=0.0;           #minimum deltal to help fit
 deltalmax=0.0;           #maximum deltal to help fit (put equal to deltalmin to leave -1.2*vmecNFP<deltal<1.2*vmecNFP)
 muc0=0.4;                #initial point for muc0
-mucMin=-0.9;             #minimum muc0 to help fit
+mucMin=0.2;             #minimum muc0 to help fit
 mucMax=0.9;              #maximum muc0 to help fit
-maxm=6;                  #Maximum poloidal Fourier mode m to output to VMEC
-maxn=7;                  #Maximum toroidal Fourier mode n to output to VMEC
-maxRecursTheta=30;       #Theta resolution in numerical integration in Mercier to VMEC
-maxRecursPhi=300;        #Phi resolution in numerical integration
+maxm=7;                  #Maximum poloidal Fourier mode m to output to VMEC
+maxn=8;                  #Maximum toroidal Fourier mode n to output to VMEC
+maxRecursTheta=40;       #Theta resolution in numerical integration in Mercier to VMEC
+maxRecursPhi=400;        #Phi resolution in numerical integration
 #======PLOTTING PARAMETERS=====
 exportBFieldSurface=1;   #0 -> Don't export figure of magnetic field on surface, 1 -> Do
-nPlotTheta=80;           #number of interpolating points in theta
-nPlotPhi=120;            #number of interpolating points in phi
-plotPointsFig=50;        #plotpoints for 3D figure
+nPlotTheta=70;           #number of interpolating points in theta
+nPlotPhi=130;            #number of interpolating points in phi
+plotPointsFig=60;        #plotpoints for 3D figure
 maxRecursPlot=2;         #max recursion for 3D figure
 ImageSizePlot=700;       #image size for 3D figure
-ImageResolutionPlot=300; #resolution for 3D figure
-nfigsSurf=3;             #number of surfaces to plot in 3D figure
+ImageResolutionPlot=400; #resolution for 3D figure
+nfigsSurf=4;             #number of surfaces to plot in 3D figure
 nPlots=4;                #number of poloidal plots to save
-npointsPolPlots=35;      #number of points for poloidal plots
+npointsPolPlots=40;      #number of points for poloidal plots
 nthetapointsBsurface=30; #plot points in theta for magnetic field on surface
 nphipointsBsurface=30;   #plot points in phi for magnetic field on surface
+coilthickness=0.10;      #thickness of the coils in VMEC units to plot
 npointsContourPlotREGCOIL=60;   #number of points in contourplot when finding coil contours in REGCOIL
-npointsInterpCoilPosREGCOIL=60; #number of points for theta grid in REGCOIL
-interpOrderCoilPosREGCOIL=3;    #interpolation order for theta grid in REGCOIL
-coilsPerHalfPeriod=3;    #number of coils per half period to plot
+npointsInterpCoilPosREGCOIL=80; #number of points for theta grid in REGCOIL
+interpOrderCoilPosREGCOIL=2;    #interpolation order for theta grid in REGCOIL
+coilsPerHalfPeriod=2;    #number of coils per half period to plot
 numHalfPeriodsToPlot=0;  #0 -> plots the whole stellarator
+plotpointsCoil=35;       #plot points in theta for coils
 #=====REGCOIL INPUT PARAMTERS========
 REGCOILtargetvalue=0.3;
-REGCOILseparation=0.08;
+REGCOILseparation=0.18;
+REGCOILnlambda=20;
 #=====TO BE IMPLEMENTED=============
 readFit=0;    		     #1 -> reads fit parameters from text file
 quasisymmetry=0;         #1 -> run quasisymmetric SENAC
@@ -103,8 +106,9 @@ if (( $runREGCOILoriginal == 1)); then
 	echo "Running REGCOIL from original VMEC file"
 	cd regcoil
 	mv regcoil_in.senac regcoil_in.senac_temp
-	head -n 19 regcoil_in.senac_temp > regcoil_in.senac
+	head -n 17 regcoil_in.senac_temp > regcoil_in.senac
 	rm regcoil_in.senac_temp
+	echo '  nlambda = '$REGCOILnlambda >> regcoil_in.senac
 	echo '  target_value = '$REGCOILtargetvalue >> regcoil_in.senac
 	echo '  separation = '$REGCOILseparation >> regcoil_in.senac
 	echo '  wout_filename = "'${vmecOutput}'"' >> regcoil_in.senac
@@ -123,8 +127,9 @@ if (( $runREGCOILfit == 1)); then
 	woutFile=$(get_abs_filename "data/${proj}/wout_${proj}_senac_ordern${ordern}_nmodes${nModes}.nc")
 	cd regcoil
 	mv regcoil_in.senac regcoil_in.senac_temp
-	head -n 19 regcoil_in.senac_temp > regcoil_in.senac
+	head -n 17 regcoil_in.senac_temp > regcoil_in.senac
 	rm regcoil_in.senac_temp
+	echo '  nlambda = '$REGCOILnlambda >> regcoil_in.senac
 	echo '  target_value = '$REGCOILtargetvalue >> regcoil_in.senac
 	echo '  separation = '$REGCOILseparation >> regcoil_in.senac
 	echo '  wout_filename = "'${woutFile}'"' >> regcoil_in.senac
@@ -139,14 +144,14 @@ if ( [[ $plotRegcoilOriginal == 1 ]] ); then
 	echo "-----------------------"
 	echo "Running SENAC Plot REGCOIL for Original Surface"
 	plotRegcoilSENAC=1;
-	wolframscript -noprompt -script src/plot_regcoil.wls $proj $ordern $nModes $ImageSizePlot $ImageResolutionPlot $coilsPerHalfPeriod $numHalfPeriodsToPlot $plotRegcoilSENAC $npointsContourPlotREGCOIL $npointsInterpCoilPosREGCOIL $interpOrderCoilPosREGCOIL $plotPointsFig $maxRecursPlot | tee -a data/${proj}/senac_${proj}_output_order${ordern}_nmodes${nModes}.txt
+	wolframscript -noprompt -script src/plot_regcoil.wls $proj $ordern $nModes $ImageSizePlot $ImageResolutionPlot $coilsPerHalfPeriod $numHalfPeriodsToPlot $plotRegcoilSENAC $npointsContourPlotREGCOIL $npointsInterpCoilPosREGCOIL $interpOrderCoilPosREGCOIL $plotPointsFig $maxRecursPlot $coilthickness $plotpointsCoil | tee -a data/${proj}/senac_${proj}_output_order${ordern}_nmodes${nModes}.txt
 fi
 
 if ( [[ $plotRegcoilFit == 1 ]]  ); then
 	echo "-----------------------"
 	echo "Running SENAC Plot REGCOIL for Fit Surface"
 	plotRegcoilSENAC=2;
-	wolframscript -noprompt -script src/plot_regcoil.wls $proj $ordern $nModes  $ImageSizePlot $ImageResolutionPlot $coilsPerHalfPeriod $numHalfPeriodsToPlot $plotRegcoilSENAC $npointsContourPlotREGCOIL $npointsInterpCoilPosREGCOIL $interpOrderCoilPosREGCOIL $plotPointsFig $maxRecursPlot | tee -a data/${proj}/senac_${proj}_output_order${ordern}_nmodes${nModes}.txt
+	wolframscript -noprompt -script src/plot_regcoil.wls $proj $ordern $nModes  $ImageSizePlot $ImageResolutionPlot $coilsPerHalfPeriod $numHalfPeriodsToPlot $plotRegcoilSENAC $npointsContourPlotREGCOIL $npointsInterpCoilPosREGCOIL $interpOrderCoilPosREGCOIL $plotPointsFig $maxRecursPlot $coilthickness $plotpointsCoil | tee -a data/${proj}/senac_${proj}_output_order${ordern}_nmodes${nModes}.txt
 fi
 
 #pkill -9 WolframKernel; pkill -9 WolframScript
